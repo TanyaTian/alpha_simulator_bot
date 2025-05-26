05c8ee6476ce56cde82a71c8a71927ab597fecb3
TanyaTian <tianyuan411249897@gmail.com>
2025-05-26 17:57:34 +0800

    修改定时器时间

--- src/process_simulated_alphas.py
+++ src/process_simulated_alphas.py
@@ -346,7 +346,7 @@ class ProcessSimulatedAlphas:
         self.logger.info("Starting daily schedule...")
         self._scheduler_running = True
         def job():
-            self.logger.info("Scheduled task triggered at 00:30.")
+            self.logger.info("Scheduled task triggered at 05:30.")
             
             # 1. 刷新date_str
             self.date_str = self.get_yesterday_date()
@@ -409,7 +409,7 @@ class ProcessSimulatedAlphas:
 
         # 清除可能存在的旧任务
         schedule.clear()
-        schedule.every().day.at("00:30").do(job).tag('daily_alpha_task')
+        schedule.every().day.at("05:30").do(job).tag('daily_alpha_task')
 
         def run_scheduler():
             while self._scheduler_running:  # 使用标志位控制循环
